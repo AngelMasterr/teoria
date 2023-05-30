08d6228f95e06e3123b65cff01d5b59e248132e5
AngelD <angelduarteing@gmail.com>
2023-05-29 19:10:23 -0500

    mas

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {Datos_personales} from './componentes/Hola_mundo';
-import {Usuarios} from './componentes/Usuarios';
-import {Prueba} from './componentes/Prueba';
+import { Datos_personales } from './componentes/Hola_mundo';
+import { Usuarios } from './componentes/Usuarios';
+import { Prueba } from './componentes/Prueba';
+import { Card } from './componentes/Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Datos_personales />
+        {/* <Datos_personales />
         <Prueba />
-        <Usuarios nombre="angel" edad={34} direccion={{calle: 'calle la libertad', ciudad: 'metropolis', pais: 'nunca jamas'}} />
+        <Usuarios
+            nombre="angel"
+            edad={34}
+            direccion={{ calle: 'calle la libertad', ciudad: 'metropolis', pais: 'nunca jamas' }}
+        /> */}
+        <Card estado={false} />
     </React.StrictMode>
 );
