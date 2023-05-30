529a74f5cfb1fc6cb49fe2b1d5932bf0e73de752
AngelD <angelduarteing@gmail.com>
2023-05-29 21:45:59 -0500

    continua learning

---
+++ src/componentes/Card_pokemon.jsx
@@ -0,0 +1,24 @@
+import './card_pokemon.css'
+const pokemon = [
+	{
+		id: 1,
+		nombre: 'pikachu',
+		imagen: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+	},
+	{
+		id: 2,
+		nombre: 'charmander',
+		imagen: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+	},
+]
+
+export function Card_pokemon() {
+	return pokemon.map((item, index) => {
+		return (
+			<div div className="card" key={index}>
+				<h1>{item.nombre}</h1>
+				<img src={item.imagen} className="imagen" />
+			</div>
+		)
+	})
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Datos_personales } from './componentes/Hola_mundo';
-import { Usuarios } from './componentes/Usuarios';
-import { Prueba } from './componentes/Prueba';
-import { Card } from './componentes/Card';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+// import { Datos_personales } from './componentes/Hola_mundo';
+// import { Usuarios } from './componentes/Usuarios';
+// import { Prueba } from './componentes/Prueba';
+// import { Card } from './componentes/Card';
+import { Card_pokemon } from './componentes/Card_pokemon'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        {/* <Datos_personales />
+	<React.StrictMode>
+		{/* <Datos_personales />
         <Prueba />
         <Usuarios
             nombre="angel"
             edad={34}
             direccion={{ calle: 'calle la libertad', ciudad: 'metropolis', pais: 'nunca jamas' }}
-        /> */}
-        <Card estado={false} />
-    </React.StrictMode>
-);
+        />
+        <Card estado={false} /> */}
+
+		<Card_pokemon />
+	</React.StrictMode>
+)
